781e4293ed79e36d38d77ff65c52f7f44d021697
REGO350 <riokanehiro228pa@gmail.com>
2021-07-07 15:47:52 +0900

    updated totalsupply

--- contracts/token/CatDoge.sol
+++ contracts/token/CatDoge.sol
@@ -34,7 +34,7 @@ contract CatDoge is ReflectiveToken {
   event OnStakingExclude(address account);
   event OnWithdrawIsolatedBNB(uint256 amount);
 
-  constructor() ReflectiveToken("CatDoge", "CATDOGE", 10**10, 9, 2, 8) {
+  constructor() ReflectiveToken("CatDoge", "CATDOGE", 10**15, 6, 2, 8) {
     _tOwned[_msgSender()] = _tTotal;
 
     // 0.03% of total supply
