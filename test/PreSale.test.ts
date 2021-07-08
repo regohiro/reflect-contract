c5bf7de3dd2861738dc97e603418b5392ec1a172
REGO350 <riokanehiro228pa@gmail.com>
2021-07-08 12:34:13 +0900

    not done yet but passed test

--- test/PreSale.test.ts
+++ test/PreSale.test.ts
@@ -1,16 +1,13 @@
+import { fromSec } from './../utilities/formatter';
+import { delay } from './../utilities/time';
 import { WBNB } from './../typechain/WBNB.d';
 import { UniswapV2Router02 } from './../typechain/UniswapV2Router02.d';
 import { UniswapV2Pair } from './../typechain/UniswapV2Pair.d';
 import { UniswapV2Factory } from './../typechain/UniswapV2Factory.d';
 import { CatDoge } from './../typechain/CatDoge.d';
+import { PreSale } from '../typechain/PreSale.d';
+import { toBN, setDefaultSigner, deployer, toUnix, toWei } from '../utilities';
 import { Signer } from 'ethers';
-import { 
-  toWei, 
-  deployer, 
-  setDefaultSigner, 
-  toBN, 
-  advanceBlockBy, 
-} from "../utilities";
 import { ethers, waffle } from "hardhat";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
@@ -24,6 +21,7 @@ describe("CatDoge Token Test", () => {
   let ownerAddr: string;
 
   let cd: CatDoge;
+  let cdps: PreSale;
 
   before(async () => {
     //Set contract accounts
@@ -45,18 +43,44 @@ describe("CatDoge Token Test", () => {
 
     //Deploy CatDoge
     cd = (await deployer("CatDoge")) as CatDoge;
+
     await cd.setRouterAddress(uniswapRouter.address);
+
+    //Set Presale args
+    const rate = (3 * 10 ** 9).toString();
+    const wallet = ownerAddr;
+    const token = cd.address;
+    const openingTime = (Math.floor(Date.now() / 1000) + 3).toString();
+    const closingTime = (toUnix("12/25/2021 13:00:00")).toString();
+    const caps = toBN(10);  //in BNB
+    const minBuyLimit = toWei(0);   //in BNB 
+
+    //Deploy Presale
+    cdps = (await deployer("PreSale", rate, wallet, token, openingTime, closingTime, caps, minBuyLimit)) as PreSale;
+
+    //Get ready for Presale
+    await cd.excludeFromFee(cdps.address);
+    await cd.excludeFromStaking(cdps.address);
+
+    const decimals = parseInt((await cd.decimals()).toString());
+    const toTransfer = toWei(10**15 / 2, decimals);
+
+    await cd.transfer(cdps.address, toTransfer);
+
+    delay(fromSec(10));
   });
 
-  describe("Check the basic things", () => {
-    it("Should display metadata and basic info correctly", async () => {
-      const tokenName = await (await (cd.name())).toString();
-      const tokenSymbol = await (await (cd.symbol())).toString();
-      const totalSupply = await (await (cd.totalSupply())).toString();
+  describe("Buy Check", () => {
+    it("Should deliver tokens to customer", async () => {
+      const toPay = toWei(0.1);
 
-      expect(tokenName).to.equal("CatDoge");
-      expect(tokenSymbol).to.equal("CATDOGE");
-      expect(parseInt(totalSupply)).to.be.greaterThan(0);
+      await cdps.connect(users[1]).buyTokens({value: toPay});
+
+      const tokenBalance = (await cd.balanceOf(usersAddr[1])).toString();
+      const decimals = parseInt((await cd.decimals()).toString());
+      const expectation = toWei(0.1 * (3 * 10 ** 9), decimals);
+
+      expect(tokenBalance).to.be.equal(expectation);
     });
   });  
 });
