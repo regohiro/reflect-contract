4e97c41c13b6d3e17626d97a4d1b41fd18f19c1d
REGO350 <riokanehiro228pa@gmail.com>
2021-08-10 17:19:55 +0900

    [add] time x amount discount feature + clean

--- contracts/presale/Presale.sol
+++ contracts/presale/Presale.sol
@@ -6,149 +6,133 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 // import "hardhat/console.sol";
 
 contract Presale is Context, Ownable, ReentrancyGuard {
-  using SafeERC20 for IERC20; 
+  using SafeERC20 for IERC20;
 
-  // The Token
+  // Instance of token to sell (DAT)
   IERC20 public token;
 
-  // Address where funds are collected
+  // Wallet where funds will be collected after withdraw
   address public wallet;
 
-  // 1BNB = [rate] tokens 
+  // 1BNB = [rate] DAT without discount
   uint256 public rate;
-
-  // Amount of wei raised
-  uint256 public weiRaised;
 
   // Presale time in UNIX
   uint256 public openingTime;
   uint256 public closingTime;
 
-  // Contributions
-  mapping(address => uint256) public contributions;
-  uint256 public caps;
-  uint256 public minBuyLimit;
+  uint256 public minBuyLimit; //0.5 * 10**18 weiBNB
+  uint256 public maxDiscount; // 3 * 10**18 weiBNB
+  uint256 public maxBounsPercentage; //25%
 
-  event TokensPurchased(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-
-
-  modifier onlyWhileOpen {
-    require(block.timestamp >= openingTime && block.timestamp <= closingTime, "Crowdsale: Presale has not started yet");
-    _;
-  }
-
+  event TokensPurchased(address indexed purchaser, uint256 value, uint256 amount);
 
   constructor(
-    uint256 _rate,
+    IERC20 _token,
     address _wallet,
-    IERC20 _token, 
-    uint256 _openingTime, 
+    uint256 _rate,
+    uint256 _openingTime,
     uint256 _closingTime,
-    uint256 _caps,
-    uint256 _minBuyLimit) {
+    uint256 _minBuyLimit,
+    uint256 _maxDiscount,
+    uint256 _maxBounsPercentage
+  ) {
+    require(_rate > 0, "Presale: rate is 0");
+    require(_openingTime >= block.timestamp, "Presale: Invalid opening time");
+    require(_closingTime >= _openingTime, "Presale: closing time < opening time");
 
-    require(_rate > 0, "Crowdsale: rate is 0");
-    require(_wallet != address(0), "Crowdsale: wallet is the zero address");
-    require(address(_token) != address(0), "Crowdsale: token is the zero address");
-    require(_openingTime >= block.timestamp, "Crowdsale: Invalid opening time");
-    require(_closingTime >= _openingTime, "Crowdsale: closing time < opening time");
-
-    caps = _caps;
-    rate = _rate;
-    wallet = _wallet;
     token = _token;
+    wallet = _wallet;
+    rate = _rate;
     openingTime = _openingTime;
     closingTime = _closingTime;
     minBuyLimit = _minBuyLimit;
+    maxDiscount = _maxDiscount;
+    maxBounsPercentage = _maxBounsPercentage;
   }
 
-  /**
-   * @return Whether crowdsale period has elapsed
-   */
   function hasClosed() public view returns (bool) {
     return block.timestamp > closingTime;
   }
 
-  /**
-   * @return Whether crowdsale is open
-   */
   function isOpen() public view returns (bool) {
     return block.timestamp >= openingTime && block.timestamp <= closingTime;
   }
 
-  function getTime() external view returns(uint256){
+  function getTime() external view returns (uint256) {
     return block.timestamp;
   }
 
-  function buyTokens() external nonReentrant payable {
-    address beneficiary = msg.sender; 
+  function buyTokens() external payable nonReentrant {
     uint256 weiAmount = msg.value;
+    address purchaser = _msgSender();
 
     //Calculate token amount to transfer
-    uint256 tokens = (weiAmount * rate) * 10**3 / 10**18;
+    uint256 tokens = getTokenAmoount(weiAmount);
 
     //Checks
-    _preValidatePurchase(beneficiary, weiAmount);
-
-    //Effects
-    weiRaised += weiAmount;
-    contributions[beneficiary] += weiAmount;
+    _preValidatePurchase(weiAmount);
 
     //Interactions
-    token.safeTransfer(beneficiary, tokens);
-    emit TokensPurchased(_msgSender(), beneficiary, weiAmount, tokens);
+    token.safeTransfer(purchaser, tokens);
+    emit TokensPurchased(purchaser, weiAmount, tokens);
   }
 
-  /**
-   * @param beneficiary Address performing the token purchase
-   * @param weiAmount Value in wei involved in the purchase
-   */
-  function _preValidatePurchase(
-    address beneficiary,
-    uint256 weiAmount
-  ) internal view onlyWhileOpen{
-    require(beneficiary != address(0), "Crowdsale: beneficiary is the zero address");
-    require(weiAmount != 0, "Crowdsale: weiAmount is 0");
-    require(minBuyLimit <= weiAmount, "Crowdsale: need to buy more");
-    require(contributions[beneficiary] + weiAmount <= caps, "Crowdsale: exceeded cap!");
+  function _preValidatePurchase(uint256 weiAmount) internal view {
+    require(block.timestamp >= openingTime && block.timestamp <= closingTime, "Presale has not started yet");
+    require(minBuyLimit <= weiAmount, "Presale: need to buy more");
   }
 
-  function setCap(uint256 _amount) external onlyOwner {
-    caps = _amount;
-  }
+  function getTokenAmoount(uint256 _weiAmount) public view returns (uint256) {
+    uint256 baseTokenAmount = _weiAmount * rate;
+    uint256 timeLeft = closingTime - block.timestamp;
+    uint256 spentAmount = _weiAmount > maxDiscount ? maxDiscount : _weiAmount;
+    uint256 maxTimeLeft = closingTime - openingTime;
+    uint256 bounsTokenAmount = ((timeLeft * spentAmount) * baseTokenAmount * maxBounsPercentage) /
+      ((maxTimeLeft * maxDiscount) * 100);
 
-  function setMinBuyLimit(uint256 _amount) external onlyOwner {
-    minBuyLimit = _amount;
+    return baseTokenAmount + bounsTokenAmount;
   }
 
   function withdrawFunds() external onlyOwner {
-    require(hasClosed(), "Crowdsale: Cannot withdraw until presale is over");
+    require(hasClosed(), "Presale: Cannot withdraw until presale is over");
     (bool success, ) = payable(wallet).call{ value: address(this).balance }("");
-    require(success, "Crowdsale: Forward funds failed");
+    require(success, "Presale: Failed to forward funds");
   }
 
   function withdrawRemainingTokens() external onlyOwner {
-    require(hasClosed(), "Crowdsale: Cannot withdraw until presale is over");
+    require(hasClosed(), "Presale: Cannot withdraw until presale is over");
     uint256 amount = token.balanceOf(address(this));
-    require(amount > 0, "Crowdsale: amount is 0");
+    require(amount > 0, "Presale: amount is 0");
     token.transfer(wallet, amount);
-  }
-
-  function setRate(uint256 _rate) external onlyOwner {
-    rate = _rate;
   }
 
   function updateWallet(address _wallet) external onlyOwner {
     wallet = _wallet;
   }
 
+  function setRate(uint256 _rate) external onlyOwner {
+    rate = _rate;
+  }
+
   function setPresaleTime(uint256 _openingTime, uint256 _closingTime) external onlyOwner {
-    require(_openingTime >= block.timestamp, "Crowdsale: Invalid opening time");
-    require(_closingTime >= _openingTime, "Crowdsale: closing time < opening time");
+    require(_openingTime >= block.timestamp, "Presale: Invalid opening time");
+    require(_closingTime >= _openingTime, "Presale: closing time < opening time");
     openingTime = _openingTime;
     closingTime = _closingTime;
+  }
+
+  function setLimits(
+    uint256 _minBuyLimit,
+    uint256 _maxDiscount,
+    uint256 _maxBounsPercentage
+  ) external onlyOwner {
+    minBuyLimit = _minBuyLimit;
+    maxDiscount = _maxDiscount;
+    maxBounsPercentage = _maxBounsPercentage;
   }
 }
